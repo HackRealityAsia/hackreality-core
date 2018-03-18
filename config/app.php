e5d8b42b2617b3e910ef28d98e2145309ea3a960
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2018-03-18 13:38:37 +0800

    Update project dependencies

--- config/app.php
+++ config/app.php
@@ -150,6 +150,9 @@ return [
         /*
          * Package Service Providers...
          */
+        Collective\Html\HtmlServiceProvider::class,
+        Vinkla\Hashids\HashidsServiceProvider::class,
+
 
         /*
          * Application Service Providers...
@@ -208,6 +211,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        // Application Package Aliases
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Hashids' => Vinkla\Hashids\Facades\Hashids::class
 
     ],
 
